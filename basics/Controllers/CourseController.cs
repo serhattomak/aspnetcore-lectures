24547e46b85aeca2d3d00ecaf6ab59cfd23acb37
Serhat Tomak <serhattomak@gmail.com>
2023-12-08 11:27:39 +0300

    Route Links

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -16,6 +16,16 @@ public class CourseController : Controller
         course.Image="1.jpg";
         return View(course);
     }
+
+    public IActionResult Details()
+    {
+        var course=new Course();
+        course.Id=2;
+        course.Title="PHP Course";
+        course.Description="PHP Course Description";
+        course.Image="2.jpg";
+        return View(course);
+    }
     //course/list
     public IActionResult List()
     {
