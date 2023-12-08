7c8e6602572df060d7a8e53867d643e2f34b2f29
Serhat Tomak <serhattomak@gmail.com>
2023-12-08 12:21:51 +0300

    Razor Syntax

--- basics/Models/Repository.cs
+++ basics/Models/Repository.cs
@@ -7,10 +7,36 @@ namespace basics.Models
         static Repository(){
             _courses=new List<Course>()
             {
-                new Course() { Id = 1, Title="ASP.NET Core Course", Description="ASP.NET Core Course Description", Image="1.jpg"},
-                new Course() { Id = 2, Title="PHP Course", Description="PHP Course Description", Image="2.jpg"},
-                new Course() { Id = 3, Title="Django Course", Description="Django Course Description", Image="3.jpg"},
-                new Course() { Id = 4, Title="JavaScript Course", Description="JavaScript Course Description", Image="4.jpg"}
+                new Course() { Id = 1, 
+                Title="ASP.NET Core Course", 
+                Description="ASP.NET Core Course Description", 
+                Image="1.jpg",
+                Tags = new string[] {"aspnet", "web development"},
+                isActive=true,
+                isHome=true
+                },
+                new Course() { Id = 2, 
+                Title="PHP Course", 
+                Description="PHP Course Description", 
+                Image="2.jpg",
+                Tags = new string[] {"php", "web development"},
+                isActive=true,
+                isHome=true
+                },
+                new Course() { Id = 3, 
+                Title="Django Course", 
+                Description="Django Course Description", 
+                Image="3.jpg",
+                isActive=true,
+                isHome=false
+                },
+                new Course() { Id = 4, 
+                Title="JavaScript Course", 
+                Description="JavaScript Course Description", 
+                Image="4.jpg",
+                isActive=true,
+                isHome=true
+                }
             };
         }
 
