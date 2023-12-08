04fde60495109ca3643e10122ab5f24b22f01d79
Serhat Tomak <serhattomak@gmail.com>
2023-12-08 14:42:21 +0300

    Controllers - Meeting App

---
+++ MeetingApp/Controllers/MeetingController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+   
+    public class MeetingController : Controller
+    {
+         public string Index()
+        {
+            return "meeting/index";
+        }
+    }
+}
